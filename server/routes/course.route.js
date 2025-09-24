f90540923c8929477993e7f5156006afe411cc1e
Sreenu-y <srinuyelesam123@gmail.com>
2025-09-25 03:30:43 +0530

    added CourseTab

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -3,6 +3,8 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
 import {
   createCourse,
+  editCourse,
+  getCourseById,
   getCreatorCourses,
 } from "../controllers/course.controller.js";
 
@@ -10,5 +12,9 @@ const router = express.Router();
 
 router.route("/").post(isAuthenticated, createCourse);
 router.route("/").get(isAuthenticated, getCreatorCourses);
+router
+  .route("/:courseId")
+  .put(isAuthenticated, upload.single("courseThumbnail"), editCourse);
+router.route("/:courseId").get(isAuthenticated, getCourseById);
 
 export default router;
